cbab1d6706a10ce2190c62e7a4b5446f36d0f87c
Nguyen Tat Tu <92445397+tattu2705@users.noreply.github.com>
2023-09-28 09:38:09 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+   var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text; 
+    var member = document.getElementById("member");
+}
